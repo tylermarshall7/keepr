fd505a740ff6871351acff5d4323142fb55344ad
tylermarshall7 <tyler.marshall7@gmail.com>
2020-09-04 15:18:17 -0600

    init

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "dev-jgxr65m3.us.auth0.com";
+export const audience = "https://keepr-api.com";
+export const clientId = "VbCcyEsvL3PsRwWXajU4qwoHCWyD1SUq";
